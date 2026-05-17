bdf15c4889082e03a3f0e81c6b888d0f8ab5d477
wezbest <110793713+wezbest@users.noreply.github.com>
2026-05-17 15:13:00 +0000

    🍫 vt1 Vite Study

--- W/vtz/vt1/src/App.jsx
+++ W/vtz/vt1/src/App.jsx
@@ -15,13 +15,13 @@ function App() {
       <Header />
       <section id="center">
         <div className="hero">
-          <img src={fem1} className="fem1" alt="Fem1 logo" />
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
           <h1>Smell Panty</h1>
+          <img src={fem1} className="fem1" alt="Fem1 logo" />
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
